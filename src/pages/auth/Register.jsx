9c9b8778d238a9979dd69359757d91c53b3163c1
tahirabbas001 <tahir.abbas@riksof.com>
2024-07-17 01:34:52 +0500

    fix

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -12,7 +12,13 @@ import AuthCarousel from "../../components/auth/AuthCarousel";
 // import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Logo from "../../Images/logo.png";
-import { UserOutlined, GithubOutlined, MailOutlined, WhatsAppOutlined, FileOutlined } from "@ant-design/icons";
+import {
+  UserOutlined,
+  GithubOutlined,
+  MailOutlined,
+  WhatsAppOutlined,
+  FileOutlined,
+} from "@ant-design/icons";
 
 const Register = () => {
   // const [loading, setLoading] = useState(false);
@@ -32,7 +38,7 @@ const Register = () => {
     };
 
     fetchContactInfo();
-  },[]);
+  }, []);
   // const onFinish = async (values) => {
   //   try {
   //     setLoading(true);
@@ -140,36 +146,73 @@ const Register = () => {
               </Button>
             </Form.Item>
           </Form> */}
+          <h1 className="text-4xl text-center font-bold mb-10">Contact Admin</h1>
           <Card
             title={contactInfo?.name}
             extra={<UserOutlined />}
-            className="max-w-sm mx-auto"
+            className="max-w-sm mx-auto shadow-2xl"
           >
             <Descriptions column={1}>
-              <Descriptions.Item label={<span><FileOutlined /> &nbsp;Portfolio&nbsp;</span>}>
+              <Descriptions.Item
+                label={
+                  <span>
+                    <FileOutlined /> &nbsp;Portfolio&nbsp;
+                  </span>
+                }
+              >
                 {contactInfo?.github && (
-                  <a href={contactInfo.portfolio} target="_blank" rel="noreferrer">
+                  <a
+                    href={contactInfo.portfolio}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     Tahir Abbas
                   </a>
                 )}
               </Descriptions.Item>
-              <Descriptions.Item label={<span><GithubOutlined /> &nbsp;Gihtub&nbsp;</span>}>
+              <Descriptions.Item
+                label={
+                  <span>
+                    <GithubOutlined /> &nbsp;Gihtub&nbsp;
+                  </span>
+                }
+              >
                 {contactInfo?.github && (
                   <a href={contactInfo.github} target="_blank" rel="noreferrer">
                     tahirabbas11
                   </a>
                 )}
               </Descriptions.Item>
-              <Descriptions.Item label={<span><WhatsAppOutlined /> &nbsp;WhatsApp&nbsp;</span>}>
+              <Descriptions.Item
+                label={
+                  <span>
+                    <WhatsAppOutlined /> &nbsp;WhatsApp&nbsp;
+                  </span>
+                }
+              >
                 {contactInfo?.contact && (
-                  <a href={`https://wa.me/+923213600429`} target="_blank" rel="noreferrer">
+                  <a
+                    href={`https://wa.me/+923213600429`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     {contactInfo.contact}
                   </a>
                 )}
               </Descriptions.Item>
-              <Descriptions.Item label={<span><MailOutlined /> &nbsp;Mail&nbsp;</span>}>
+              <Descriptions.Item
+                label={
+                  <span>
+                    <MailOutlined /> &nbsp;Mail&nbsp;
+                  </span>
+                }
+              >
                 {contactInfo?.contact && (
-                  <a href={`mailto:${contactInfo.email}`} target="_blank" rel="noreferrer">
+                  <a
+                    href={`mailto:${contactInfo.email}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     {contactInfo.email}
                   </a>
                 )}
@@ -183,7 +226,7 @@ const Register = () => {
             </Link>
           </div>
         </div>
-        <div className="sm:flex hidden xl:w-4/6 min-w-[500px] bg-[#6c63ff]" >
+        <div className="sm:flex hidden xl:w-4/6 min-w-[500px] bg-[#6c63ff]">
           <div className="w-full mt-40">
             <Carousel autoplay>
               <AuthCarousel
